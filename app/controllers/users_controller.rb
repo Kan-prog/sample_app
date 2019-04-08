2a51eb1d672e7cba1ef8ed85884f3a7839977631
kan <pekkanseig8@gmail.com>
2019-04-08 22:47:17 +0000

    finish user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+  
   def show
     @user = User.find(params[:id])
     # debugger
@@ -15,9 +18,23 @@ class UsersController < ApplicationController
       flash[:success] = "ユーザー登録に成功しました！"
       redirect_to @user
     else
-      flash[:danger] = "入力内容が正しくありません。"
+      # flash[:danger] = "入力内容が正しくありません。"
       render "new"
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールを更新しました！"
+      redirect_to @user
+    else
+      render 'edit'
+    end  
   end
 
   private
@@ -25,5 +42,23 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+     # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        flash[:danger] = "権限がありません。"
+        redirect_to(root_url)
+      end  
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  # get 'sessions/new'
 
-  get 'users/new'
+  # get 'users/new'
 
   root 'static_pages#home'
 
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   
-  resources :users, only: [:create, :show]
+  resources :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
