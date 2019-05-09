45376faf288cb35e80db354a42b0988ed5ef3340
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2019-05-09 05:53:32 +0000

    rename heroku app

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :receive_user, class_name: 'User'
   has_many :notifications,dependent: :destroy
-  default_scope -> { order(created_at: :desc) }  
+  # default_scope -> { order(created_at: :desc) }  
   # メッセージが送信されたとき(messages#createが実行されたとき)に、notificationテーブルにメール通知が登録される
   def create_notification_by(current_user)
     notification = current_user.active_notifications.new(
