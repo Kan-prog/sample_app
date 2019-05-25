8ea31cd6aa8ccde031a5f4248846b9f1bed0344f
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2019-05-25 07:29:06 +0000

    carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,8 @@ if Rails.env.production?
       path_style: true
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/unitra"
+    config.cache_storage = :fog
     # config.asset_host = 'https://kan-unitra.s3.amazonaws.com'
   end
 end
