ff240113efcf7057a3b871441e862e9ee6765bef
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2019-09-27 06:54:31 +0000

    asset_host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    # config.fog_provider = 'fog/aws'
+    
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.fog_credentials = {
       # Amazon S3用の設定
@@ -15,9 +15,11 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       path_style: true
     }
+    config.fog_provider =       'fog/aws'
     config.fog_directory     =  ENV['S3_BUCKET']
     config.fog_attributes =     { 'Cache-Control' => "max-age=#{365.day.to_i}" }
-    config.asset_host =         "https://s3.us-east-1.amazonaws.com/tradents"
+    # config.asset_host =         "https://s3.us-east-1.amazonaws.com/tradents"
+    config.asset_host = 'https://tradents1.s3.amazonaws.com' #このように修正
     config.cache_storage =      :fog
     config.fog_public =         true
   end
