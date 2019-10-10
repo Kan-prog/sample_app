d02192e51f9811585f876ebc31946c01b23c7e47
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2019-10-10 16:18:38 +0000

    admin/college,genres

--- app/admin/colleges.rb
+++ app/admin/colleges.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register College do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :last_email
+  permit_params :id, :name, :last_email
   #
   # or
   #
