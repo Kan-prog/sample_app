e9940601ae2a989207ca0e52e0354b2bfa8d4244
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2019-10-10 02:13:04 +0000

    controllers/admin_user

--- app/controllers/active_admin.rb
+++ app/controllers/active_admin.rb
@@ -4,9 +4,12 @@ ActiveAdmin.register User do
 
   controller do
     def access_denied #管理者画面へのアクセス拒否挙動
-      if current_user && current_user.password == admin_user_url.password && current_user.email == admin_user_url.password
+      admin_user = AdminUser.find(id: 1)
+      # if current_user && current_user.password == admin_user_url.password && current_user.email == admin_user_url.password
+      if current_user && current_user.password == admin_user.password && current_user.email == admin_user.password
         redirect_to admin_root_path, alert: I18n.t('active_admin.access_denied.message')
-      elsif current_user && (current_user.password != admin_user_url.password || current_user.email != admin_user_url.password)
+      # elsif current_user && (current_user.password != admin_user_url.password || current_user.email != admin_user_url.password)
+      elsif current_user && (current_user.password != admin_user.password || current_user.email != admin_user.password)
         redirect_to root_path, alert: "アクセスできません"
       else
         redirect_to login_path, alert: "ログインしてください"
