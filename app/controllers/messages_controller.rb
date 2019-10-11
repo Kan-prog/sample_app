5872a845072afa6a79710fbd4f51cd1a52acffa9
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2019-10-11 07:58:57 +0000

    before_Action in messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
   
+  before_action :logged_in_user, only: [:index, :create, :destroy]
+  
   def index
     @user = User.find(params[:user_id])
     # ログインユーザーが送ったメールのidの中から、あるユーザー@userのidを探す
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  before_action :logged_in_user
+  
   include NotificationsHelper
   def index
     @notifications = current_user.passive_notifications.paginate(page: params[:page], per_page: 10).includes(:message, :micropost)
