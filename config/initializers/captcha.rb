3c1ecfbaa2d7e5743fdc1786dca2fce35528d180
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2019-10-17 13:25:45 +0000

    captcha

--- config/initializers/captcha.rb
+++ config/initializers/captcha.rb
@@ -1,5 +1,7 @@
-Recaptcha.configure do |config|
-  config.site_key  = ENV['CAPTCHA_SITE_KEY']
-  config.secret_key = ENV['CAPTCHA_SECRET_KEY']
-  config.skip_verify_env += %w[development]
-end
+if Rails.env.production?
+  Recaptcha.configure do |config|
+    config.site_key  = ENV['CAPTCHA_SITE_KEY']
+    config.secret_key = ENV['CAPTCHA_SECRET_KEY']
+    config.skip_verify_env += %w[development]
+  end
+end  
