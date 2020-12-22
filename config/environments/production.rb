ac6ba6bcd502733747d9f075d70cce674ff0b567
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2020-12-22 11:05:32 +0000

    sendgrid error slove

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
+    # :user_name      => ENV['SENDGRID_USERNAME'],
+    :user_name      => ENV['SENDGRID_API_KEY'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
