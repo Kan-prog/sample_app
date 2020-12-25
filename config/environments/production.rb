8e85173f6cf12e9b0ff5bf865ea8fc2aa555cf7e
EC2 Default User <ec2-user@ip-172-31-84-15.ec2.internal>
2020-12-25 06:16:08 +0000

    sendgrid login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    # :user_name      => ENV['SENDGRID_USERNAME'],
-    :user_name      => ENV['SENDGRID_API_KEY'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    # :user_name      => ENV['SENDGRID_API_KEY'],
     :password       => ENV['SENDGRID_PASSWORD2'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
